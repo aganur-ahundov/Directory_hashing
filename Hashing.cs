b7f60b03d530b1c32398d80599ee8b386c5426fe
aganurych <aganurych@gmail.com>
2017-07-01 12:06:39 +0300

    hashing v1

---
+++ Hashing.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GLogic_hashing
+{
+    public class Hashing
+    {
+        //Which directory will be hashed
+        private DirectoryInfo _directoryForHashing;
+
+        //File for results
+        private FileStream _outputFile;
+
+        private SHA256Managed _hash = new SHA256Managed();
+
+        public Hashing( string directoryPath, string outputFilePath )
+        {
+            _directoryForHashing = new DirectoryInfo( directoryPath );
+            _outputFile = new FileStream( outputFilePath, FileMode.OpenOrCreate );
+        }
+
+        public void ComputeHash()
+        {
+            //_directoryForHashing.GetDirectories().AsParallel
+            ComputeDirectory( _directoryForHashing );
+        }
+
+        private byte[] ComputeFileHash( string path )
+        {
+            FileStream fs = new FileStream( path, FileMode.Open, FileAccess.Read );
+            byte[] hashBytes = _hash.ComputeHash( fs );
+
+            fs.Close();
+
+            return hashBytes;
+        }
+
+        private void ComputeDirectory( DirectoryInfo _directory )
+        {
+            _directory.GetFiles().AsParallel().ForAll( f => 
+            {
+                byte[] fileNameBytes = ASCIIEncoding.UTF8.GetBytes( " " + f.Name );
+                byte[] hashBytes = ComputeFileHash( f.FullName );
+                byte[] result = hashBytes.Concat( fileNameBytes ).ToArray();
+
+                _outputFile.WriteAsync( result, 0, result.Length );
+            }
+            );
+
+            //_directory.GetDirectories().AsParallel
+            foreach ( DirectoryInfo directory in _directory.GetDirectories() )
+            {
+                ComputeDirectory( directory );
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace GLogic_hashing
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            SHA256Managed sha = new SHA256Managed();
+            FileStream fs = new FileStream(@"C:\Users\Aganurych\Desktop\pict.jpg", FileMode.Open);
+            FileStream fsWrite = new FileStream(@"C:\Users\Aganurych\Desktop\Hash.txt", FileMode.OpenOrCreate );
+
+            byte[] hash = sha.ComputeHash( fs );
+            Task t = fsWrite.WriteAsync( hash, 0, hash.Length );
+            t.Wait();
+
+            fs.Close();
+            fsWrite.Close();
+            sha.Dispose();
+          
+        }
+    }
+}
