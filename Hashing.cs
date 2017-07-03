c7bab5eb9cbabd02331153bc1ba3cc2c1689f7d3
aganurych <aganurych@gmail.com>
2017-07-03 12:31:12 +0300

    Hashing v3

--- Hashing.cs
+++ Hashing.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
-using System.Threading;
 using System.Diagnostics;
 
 
@@ -38,9 +36,9 @@ namespace GLogic_hashing
         //Collect all tasks which computes files hash
         private List<Task> allTasks = new List<Task>();
 
-
-        public int totalTime { get; set; }
         public long totalBytes { get; set; }
+
+
 
 
         public Hashing( string directoryPath, string outputFilePath )
@@ -52,105 +50,93 @@ namespace GLogic_hashing
 
         public void ComputeHash()
         {
-            //CLEAR HERE
-            Stopwatch sw1 = new Stopwatch();
-            sw1.Start();
-            ComputeDirectory( _directoryForHashing );
-            sw1.Stop();
-            Console.WriteLine( $"Compute Directory: {sw1.ElapsedMilliseconds} ms" );
-
-            sw1.Restart();
-            var tmp = allTasks.Where( t => t.Status == TaskStatus.RanToCompletion ).Count();
-            Task.WaitAll( allTasks.ToArray() );
-            sw1.Stop();
-            Console.WriteLine( $"Wait All: { sw1.ElapsedMilliseconds } ms" );
-
-            sw1.Restart();
+            ComputeHashInDirectory( _directoryForHashing );
             SaveData();
-            sw1.Stop();
-            Console.WriteLine( $"Save Data: { sw1.ElapsedMilliseconds } ms" );
-
         }
 
 
-        private Task  ComputeFileHash( FileStream fStream )
+        private void CollectHash( FileStream fStream )
         {
-           return Task.Run( () => 
-           {
-               //Get total length for calculating performance
-               totalBytes += fStream.Length;
+            //FileInfo for get name of file without path
+            FileInfo info = new FileInfo(fStream.Name);
 
-               //FileInfo for get name of file without path
-               FileInfo info = new FileInfo( fStream.Name );
-
-               //Add pair: file name - file hash 
-               _result.Add( new HashingResult
-               {
-                   _fileName = info.Name,
-                   _hash = Encoding.Default.GetString( _hash.ComputeHash( fStream ))
-               } );
-
-               //?????
-               totalTime += Process.GetCurrentProcess().TotalProcessorTime.Milliseconds;
-           });
-        }
-
-
-        private void ComputeDirectory( DirectoryInfo _directory )
-        {
-            List<Task> taskList = new List<Task>();
-            List<FileStream> fStreamList = new List<FileStream>();
-
-            try
+            //Add pair: file name - file hash 
+            _result.Add( new HashingResult
             {
+                _fileName = info.Name,
+                _hash = Encoding.Default.GetString(_hash.ComputeHash(fStream))
+            });
+
+            //Get total length for calculating performance
+            totalBytes += fStream.Length;
+            
+        }
+
+        private Task CollectAllFileHash( DirectoryInfo _directory )
+        {
+            return Task.Run( () =>
+            {
+                List<FileStream> fStreamList = new List<FileStream>();
                 foreach ( var file in _directory.GetFiles() )
                 {
                     FileStream fStream = new FileStream( file.FullName, FileMode.Open, FileAccess.Read );
 
                     //Save all FileStream for closing it later
                     fStreamList.Add( fStream );
-
-                    
-                    Task task = ComputeFileHash( fStream );
-                    taskList.Add( task );
+                    CollectHash( fStream );
                 }
 
-                //Save all tasks for checking if all of them are completed
-                allTasks.AddRange( taskList );
-                
-                //Close FileStreams, after all tasks are completed
-                Task.WhenAll( taskList.ToArray() ).ContinueWith( ( Task t ) =>
-                {
-                    foreach ( var stream in fStreamList )
-                      stream.Close();
-                });
+                //Close all streams
+                foreach ( var stream in fStreamList )
+                    stream.Close();
+            });
+        }
 
+        private void ComputeHashInDirectory( DirectoryInfo _directory )
+        {
+            //Recursively for all directories
+            foreach ( DirectoryInfo directory in _directory.GetDirectories() )
+            {
+                ComputeHashInDirectory( directory );
+            }
+            
+
+            try
+            {
+                //Collect all tasks
+                Task t = CollectAllFileHash( _directory );
+                allTasks.Add( t );
             }
             catch ( Exception e )
             {
                 Console.WriteLine( e.GetType().FullName );
                 Console.WriteLine( e.Message );
             }
-
-
-            //Recursively for all directories
-            foreach ( DirectoryInfo directory in _directory.GetDirectories() )
-            {
-                ComputeDirectory( directory );
-            }
+            
         }
+        
 
         private void SaveData()
         {
             int it = 1;
             using ( StreamWriter writer = new StreamWriter( _outputFilePath, false, Encoding.Default ) )
             {
-                //Save all pairs in the file
-                foreach ( var pair in _result )
-                {
-                    writer.WriteLine( $"{it}.  {pair._fileName} \t\t {pair._hash}" );
-                    it++;
-                }
+                //Complete all tasks
+                Task.WhenAll( allTasks )
+                    .ContinueWith( ( Task t ) =>
+                 { 
+                     //Save all pairs in the file
+                    foreach ( var pair in _result )
+                    {
+                        writer.WriteLine( $"{it}.  {pair._fileName} \t\t {pair._hash}" );
+                        it++;
+                    }
+
+                    //Performance calculation (allDataSize/totalProcessorTime)
+                     double procTime = Process.GetCurrentProcess().TotalProcessorTime.TotalSeconds;
+                     writer.WriteLine( string.Format( "Performance: {0:0.00} MB/s (by CPU time)", (totalBytes/MB) / procTime ));
+
+                } ).Wait();
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,38 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
-using System.IO;
-using System.Diagnostics;
-using GLogic_hashing;
 
 namespace GLogic_hashing
 {
     class Program
     {
-        static void Main( string[] args ) 
+        static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-
-            Hashing hash = new Hashing(@"C:\Users\Aganurych\Desktop\Test", @"C:\Users\Aganurych\Desktop\Output.txt");
             try
             {
-                //Hashing hash = new Hashing( args[0], args[1] );
+                Hashing hash = new Hashing( args[0], args[1] );
                 hash.ComputeHash();
-
             }
             catch ( Exception e )
             {
                 Console.WriteLine( e.Message );
             }
-            sw.Stop();
-            Console.WriteLine( $"Total Time: {sw.ElapsedMilliseconds} ms" );
-            Console.WriteLine( $"Total Processor Time: { Process.GetCurrentProcess().TotalProcessorTime.Milliseconds } ms");
-            Console.WriteLine( $"User Processor Time: { Process.GetCurrentProcess().UserProcessorTime.Milliseconds } ms" );
-            Console.WriteLine( $"Absolute: { hash.totalTime } ms" );
+
         }
     }
 }
